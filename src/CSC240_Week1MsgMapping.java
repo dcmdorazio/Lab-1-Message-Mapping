a9893b3aca48d47278ad0cd2c00787cc6cf17a9f
Cody D'Orazio <cxdorazio@gmail.com>
2019-09-01 20:51:15 -0400

    Initial Commit: 09/01/19 Commit Log: 09/01/19: Created Files for the initialization of the application.

--- src/CSC240_Week1MsgMapping.java
+++ src/CSC240_Week1MsgMapping.java
@@ -1,10 +1,10 @@
 /*
- * Developer: Cody D'Orazio
- * Class: Computer Science III (CSC240)
- * Assigned: 08/29/19
- * Lab 1: Message Mapping Scheme
- * Description: This IntelliJ application will take the given .txt file and
- * Last Edited: 09/01/19
+* Developer: Cody D'Orazio
+* Class: Computer Science III (CSC240)
+* Assigned: 08/29/19
+* Lab 1: Message Mapping Scheme
+* Description: This IntelliJ application will take the given .txt file and
+* Last Edited: 09/01/19
  */
 
 import java.io.File;
@@ -14,18 +14,13 @@ import java.util.Scanner;
 public class CSC240_Week1MsgMapping {
 
     String text = "";
-    private File message = new File("/message.txt");
+    private File message = new File ("/message.txt");
 
-    try
-
-    {
+    try {
         Scanner msgScan = new Scanner(message);
         msg = msgScan.next();
     }
-    catch(
-    FileNotFoundException c)
-
-    {
+    catch (FileNotFoundException c) {
         c.printStackTrace();
         msg = c.getLocalizedMessage();
     }
