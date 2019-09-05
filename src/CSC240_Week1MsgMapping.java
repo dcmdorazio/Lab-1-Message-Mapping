bbd2460d3c573e0f9d3af6ba7f0c422a6e1701d4
Cody D'Orazio <44353948+dcmdorazio@users.noreply.github.com>
2019-09-05 15:10:04 -0400

    Initial Commit: 09/01/19
    Commit Log:
    09/01/19: Created Files for the initialization of the application.
    09/03/19: Code is re-arranged to accept the incoming file code.
    09/03/19: Had to start over because the code was incorrect.
    09/04/19: Moved 'Main' method from its own java class, to the MsgMapping.java class.

--- src/CSC240_Week1MsgMapping.java
+++ src/CSC240_Week1MsgMapping.java
@@ -9,27 +9,41 @@
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CSC240_Week1MsgMapping {
     public static void main(String[] args) throws IOException {
 
         /*TODO: WHEN TRYING TO RUN THIS FILE, MAKE SURE YOU CHANGE THE PATHNAME OF THE .txt FILE
-        *  TO WHERE YOU SAVED IT. THIS IS CRUCIAL IN ORDER TO HAVE THIS PROGRAM FUNCTION PROPERLY! */
+         *  TO WHERE YOU SAVED IT. THIS IS CRUCIAL IN ORDER TO HAVE THIS PROGRAM FUNCTION PROPERLY! */
         //File file = new File("C:\\Users\\cxdor\\IdeaProjects\\CSC240_Week1MsgMapping\\src\\message.txt");
+
+        //Scanner scan = new Scanner(System.in);
+        Scanner input = new Scanner(fileName);
+        String fileName = input.nextLine();
         File file = new File(fileName);
-
-        Scanner input = new Scanner(file);
-
-        Scanner c = new Scanner(file);
 
 
         // This function will have the program ask for user input
         System.out.println("Enter the name of the file you wish to use:");
 
-        while (c.hasNextLine()) {
-            System.out.println(c.nextLine());
+        while (input.hasNextLine()) {
+            System.out.println(input.nextLine());
+            String st = input.next();
+            //TODO: Activate "words" variable and ArrayList.
+            ArrayList<String> words = new ArrayList<>(Arrays.asList(st));
 
+
+            /* This for loop uses integers 'c' and 'd' to compare the words
+            * in the list, and eventually print them out*/
+            for (int c =0; i < words.size(); i++) {
+                for(int d = c + 1; d <words.size(); d++) {
+                    if (words.get(c).equals(words.get(d))) words.remove(c);
+                }
+            }
+            System.out.println(words);
         }
 
 
