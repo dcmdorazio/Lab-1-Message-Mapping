7da5cf22f26d9c33322c455e99b8a8ba976f15fd
Cody D'Orazio <44353948+dcmdorazio@users.noreply.github.com>
2019-09-05 01:36:40 -0400

    Initial Commit: 09/01/19
    Commit Log:
    09/01/19: Created Files for the initialization of the application.
    09/03/19: Code is re-arranged to accept the incoming file code.
    09/03/19: Had to start over because the code was incorrect.
    09/04/19: Moved 'Main' method from its own java class, to the MsgMapping.java class.

--- src/CSC240_Week1MsgMapping.java
+++ src/CSC240_Week1MsgMapping.java
@@ -4,26 +4,35 @@
  * Assigned: 08/29/19
  * Lab 1: Message Mapping Scheme
  * Description: This IntelliJ application will take the given .txt file and
- * Last Edited: 09/01/19
+ * Last Edited: 09/04/19
  */
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class CSC240_Week1MsgMapping {
-public static void main (String[] args) throws IOException {
-    //File file = new File("C:\\Users\\cxdor\\IdeaProjects\\CSC240_Week1MsgMapping\\src\\resources\\message.txt");
-    File file = new File("/message.txt");
-    Scanner scan = new Scanner(file);
+    public static void main(String[] args) throws IOException {
 
-    while (scan.hasNextLine())
-        System.out.println(scan.nextLine());
+        /*TODO: WHEN TRYING TO RUN THIS FILE, MAKE SURE YOU CHANGE THE PATHNAME OF THE .txt FILE
+        *  TO WHERE YOU SAVED IT. THIS IS CRUCIAL IN ORDER TO HAVE THIS PROGRAM FUNCTION PROPERLY! */
+        //File file = new File("C:\\Users\\cxdor\\IdeaProjects\\CSC240_Week1MsgMapping\\src\\message.txt");
+        File file = new File(fileName);
+
+        Scanner input = new Scanner(file);
+
+        Scanner c = new Scanner(file);
 
 
-}
+        // This function will have the program ask for user input
+        System.out.println("Enter the name of the file you wish to use:");
+
+        while (c.hasNextLine()) {
+            System.out.println(c.nextLine());
+
+        }
 
 
+    }
 }
 
