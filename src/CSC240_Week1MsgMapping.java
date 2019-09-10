8e5b3036b74293bb6b633a3bf8671a0fca47a8d6
Cody D'Orazio <cxdorazio@gmail.com>
2019-09-10 19:02:12 -0400

    Initial Commit: 09/01/19
    Commit Log:
    09/01/19: Created Files for the initialization of the application.
    09/03/19: Code is re-arranged to accept the incoming file code.
    09/03/19: Had to start over because the code was incorrect.
    09/04/19: Moved 'Main' method from its own java class, to the MsgMapping.java class.
    09/09/19: Refurbished "runner.java"(MAIN METHOD). Implimented new methods
              to CSC240_Week1MsgMapping.java.
    09/10/19: Fixed the remainding methods and allowed the output to print.

--- src/CSC240_Week1MsgMapping.java
+++ src/CSC240_Week1MsgMapping.java
@@ -4,53 +4,19 @@
  * Assigned: 08/29/19
  * Lab 1: Message Mapping Scheme
  * Description: This IntelliJ application will take the given .txt file and
- * Last Edited: 09/09/19
+ * Last Edited: 09/10/19
  */
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class CSC240_Week1MsgMapping {
 
-    public static void main(String[] args) throws IOException {
-        ArrayList<String> list = new ArrayList<>();
-
-        /*This scanner, called "c", is what I decided to name my scanner funtion. */
-        Scanner c = new Scanner(System.in);
-
-        System.out.print("Enter the name of the file you wish to use: ");
-        String fileName = c.nextLine();
-        File file = new File(fileName);
-
-        /*This scanner called "input" will be used to take
-         * the users input into the machine for use.*/
-        Scanner input = new Scanner(file);
-        //int counter = 0;
-
-        /* The while and for loop below, is used to process the words
-         * in the list and print them out in "List Formation". */
-        while (input.hasNextLine()) {
-            list.add(input.next());
-        }
-        for (String cd : list) {
-            System.out.println(cd);
-        }
-    }
-    /*reads a list puts it into a new list, and reads off the old list for duplicates
-    and re-attatches it to the second list.*/
-    // using the contains() method for the statement above
-
-
-    public static String[] removeDuplicate(String[] words){
+    public static String[] removeDuplicate(String[] words) {
         // remember which word is a duplicate
         boolean[] isDuplicate = new boolean[words.length];
         // and count them
         int countDuplicate = 0;
-        for (int i = 0; i < words.length ; i++){
+        for (int i = 0; i < words.length; i++) {
             // only check "forward" because "backwards checked" duplicates have been marked yet
-            for(int j = i + 1; j < words.length ; j++){
+            for (int j = i + 1; j < words.length; j++) {
                 if (words[i].equals(words[j])) {
                     isDuplicate[j] = true;
                     countDuplicate++;
@@ -70,7 +36,6 @@ public class CSC240_Week1MsgMapping {
         return lastList;
     }
 
-
 //TODO: Have an array that counts the longest line in the txt file
 
    /* private static void removeDuplicateWords(ArrayList<String> wordList) {
@@ -84,11 +49,11 @@ public class CSC240_Week1MsgMapping {
         }
     }*/
 
-/* END OF CSC240_WEEK1MSGMAPPING.JAVA */
+    /* END OF CSC240_WEEK1MSGMAPPING.JAVA */
 }
 
 /* THE COMMENTS BELOW ARE LEFT IN THIS FILE BECAUSE THEY INCLUDE OLD / IN PROGRESS
-*  IDEAS. THESE WERE ALSO USED TO TEST MY IDEAS WITH MY ACTUAL RUNNING CODE*/
+ *  IDEAS. THESE WERE ALSO USED TO TEST MY IDEAS WITH MY ACTUAL RUNNING CODE*/
 
 /**/ /**/ /**/ /**/ /**/ /*PLEASE*/ /*SEE*/ /*BELOW*/ /**/ /**/ /**/ /**/ /**/ /**/ /**/ /**/ /**/ /**/
 
