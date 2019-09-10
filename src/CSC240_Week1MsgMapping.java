1a660b112630cefea64d4a1715c106c8db4b0463
Cody D'Orazio <cxdorazio@gmail.com>
2019-09-09 23:07:25 -0400

    Initial Commit: 09/01/19
    Commit Log:
    09/01/19: Created Files for the initialization of the application.
    09/03/19: Code is re-arranged to accept the incoming file code.
    09/03/19: Had to start over because the code was incorrect.
    09/04/19: Moved 'Main' method from its own java class, to the MsgMapping.java class.
    09/09/19: Refurbished "runner.java"(MAIN METHOD). Implimented new methods
              to CSC240_Week1MsgMapping.java.
    09/10/19:

--- src/CSC240_Week1MsgMapping.java
+++ src/CSC240_Week1MsgMapping.java
@@ -4,20 +4,23 @@
  * Assigned: 08/29/19
  * Lab 1: Message Mapping Scheme
  * Description: This IntelliJ application will take the given .txt file and
- * Last Edited: 09/04/19
+ * Last Edited: 09/09/19
  */
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class CSC240_Week1MsgMapping {
+    /*reads a list puts it into a new list, and reads off the old list for duplicates
+    and re-attatches it to the second list.*/
+    // using the contains() method for the statement above
 
     //TODO: Make a 2D array
     //TODO: Make array to store words (non duplicate)
+    //TODO: Have an array that counts the longest line in the txt file
+    //TODO: Make a loop that scans the document
+
 
     public static boolean removeDuplicateWords(ArrayList<String> wordList) {
         for (int i = 0; i < wordList.size(); i++) {
@@ -33,19 +36,19 @@ public class CSC240_Week1MsgMapping {
 
 
     // Removes duplicate words from original "List"
-    public static <String> ArrayList<String> removeDuplicates(ArrayList<String> list) {
-        // Creates new list
-        ArrayList<String> newList = new ArrayList<String>();
+    public static <String> ArrayList<String> removeDups(ArrayList<String> list) {
+
+        // Creates a new arrayList called "nextList"
+        ArrayList<String> nextList = new ArrayList<String>();
+
         // Scans old list, and places non duplicated words into new list.
         for (String word : list) {
-
-            // If this word is not in newList, add it
+            // If this word is not in the list then add that sucker into the array!!
             if (!list.contains(word)) {
-
-                newList.add(word);
+                nextList.add(word);
             }
         }
-
+//TODO: Left of here
         // return the new list, non duplicated words list
         return list;
     }
@@ -56,49 +59,30 @@ public class CSC240_Week1MsgMapping {
         }
 
     } */
-   private input() {
+
+    private input() {
+
+        Scanner input = new Scanner(file);
 
 
-       Scanner input = new Scanner(file);
+        while (input.hasNextLine()) {
+            System.out.println(input.nextLine());
+            String st = input.next();
 
+            //TODO: Activate "words" variable and ArrayList.
+            ArrayList<String> words = new ArrayList<>(Collections.singletonList(st));
 
-       while (input.hasNextLine()) {
-           System.out.println(input.nextLine());
-           String st = input.next();
-
-           //TODO: Activate "words" variable and ArrayList.
-           ArrayList<String> words = new ArrayList<>(Collections.singletonList(st));
-
-           /* This for loop uses integers 'c' and 'd' to compare the words
-            * in the list, and eventually print them out*/
-           for (int c = 0; c < words.size(); c++) {
-               for (int d = c + 1; d < words.size(); d++) {
-                   if (words.get(c).equals(words.get(d))) words.remove(d);
-               }
-           }
-           System.out.println(words);
-       }
-       return;
-   }
-
-    public static void main(String[] args) throws IOException {
-
-        /*TODO: WHEN TRYING TO RUN THIS FILE, MAKE SURE YOU CHANGE THE PATHNAME OF THE .txt FILE
-         *  TO WHERE YOU SAVED IT. THIS IS CRUCIAL IN ORDER TO HAVE THIS PROGRAM FUNCTION PROPERLY! */
-        //File file = new File("C:\\Users\\cxdor\\IdeaProjects\\CSC240_Week1MsgMapping\\src\\message.txt");
-        Scanner cd = new Scanner(System.in);
-        // This function will have the program ask for user input
-        System.out.print("Enter the name of the file you wish to use:");
-        String fileName = cd.nextLine();
-
-        File file = new File(fileName);
-
+            /* This for loop uses integers 'c' and 'd' to compare the words
+             * in the list, and eventually print them out*/
+            for (int c = 0; c < words.size(); c++) {
+                for (int d = c + 1; d < words.size(); d++) {
+                    if (words.get(c).equals(words.get(d))) words.remove(d);
+                }
+            }
+            System.out.println(words);
+        }
+        return;
     }
-
-//    @Override
-//    public int hashCode() {
-//        return super.hashCode();
-//    }
 
 
 }
