fb6ef9958d87c2fbaf9828aba45fb27b1a1d88a2
Cody D'Orazio <cxdorazio@gmail.com>
2019-09-10 21:17:02 -0400

    Initial Commit: 09/01/19
    Commit Log:
    09/01/19: Created Files for the initialization of the application.
    09/03/19: Code is re-arranged to accept the incoming file code.
    09/03/19: Had to start over because the code was incorrect.
    09/04/19: Moved 'Main' method from its own java class, to the MsgMapping.java class.
    09/09/19: Refurbished "runner.java"(MAIN METHOD). Implimented new methods
              to CSC240_Week1MsgMapping.java.
    09/10/19: Fixed the output once the user types their .txt file.

--- src/CSC240_Week1MsgMapping.java
+++ src/CSC240_Week1MsgMapping.java
@@ -7,55 +7,59 @@
  * Last Edited: 09/10/19
  */
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CSC240_Week1MsgMapping {
 
-    public static String[] removeDuplicate(String[] words) {
-        // remember which word is a duplicate
-        boolean[] isDuplicate = new boolean[words.length];
-        // and count them
-        int countDuplicate = 0;
-        for (int i = 0; i < words.length; i++) {
-            // only check "forward" because "backwards checked" duplicates have been marked yet
-            for (int j = i + 1; j < words.length; j++) {
-                if (words[i].equals(words[j])) {
-                    isDuplicate[j] = true;
-                    countDuplicate++;
-                }
-            }
+    public static void main(String[] args) throws IOException {
+        //First array in project
+        ArrayList<String> list = new ArrayList<>();
+
+        /*This scanner, called "c", is what I decided to name my scanner funtion. */
+        Scanner c = new Scanner(System.in);
+
+        /* The 3 lines of code below, are to take the users input to parse the .txt file
+        ** the user inputs with they keyboard*/
+        System.out.print("Enter the name of the file you wish to use: ");
+        String fileName = c.nextLine();
+        File file = new File(fileName);
+
+        /*This scanner called "input" will be used to take
+         * the users input into the machine for use.*/
+        Scanner input = new Scanner(file);
+        //int counter = 0;
+
+        while (input.hasNext()) {
+            list.add(input.next());
         }
-        // collect non-duplicate strings
-        String[] lastList = new String[words.length - countDuplicate];
-        int j = 0;
-        for (int i = 0; i < isDuplicate.length; i++) {
-            if (!isDuplicate[i]) {
-                lastList[j] = words[i];
-                j++;
-            }
+        ArrayList<String> nextList = CSC240_Week1MsgMapping.removeDups(list);
+        for(String x : nextList) {
+            System.out.println(x);
         }
-        // and return them
-        return lastList;
+
     }
 
-//TODO: Have an array that counts the longest line in the txt file
+    private static <String> ArrayList<String> removeDups(ArrayList<String> list) {
+        //Second array in project
+        ArrayList<String> nextList = new ArrayList<>();
 
-   /* private static void removeDuplicateWords(ArrayList<String> wordList) {
-        for (int i = 0; i < wordList.size(); i++) {
-            for (int j = i + 1; j < wordList.size(); j++) {
-                if (wordList.get(i).equals(wordList.get(j))) {
-                    return;
-                }
+        for (String words : list) {
+            if (!nextList.contains(words)) {
+                nextList.add(words);
             }
-
         }
-    }*/
-
-    /* END OF CSC240_WEEK1MSGMAPPING.JAVA */
+        return nextList;
+    }
 }
 
-/* THE COMMENTS BELOW ARE LEFT IN THIS FILE BECAUSE THEY INCLUDE OLD / IN PROGRESS
- *  IDEAS. THESE WERE ALSO USED TO TEST MY IDEAS WITH MY ACTUAL RUNNING CODE*/
-
-/**/ /**/ /**/ /**/ /**/ /*PLEASE*/ /*SEE*/ /*BELOW*/ /**/ /**/ /**/ /**/ /**/ /**/ /**/ /**/ /**/ /**/
+/*THE COMMENTED CODE BELOW THIS LINE ARE TEST METHODS AND
+* OTHER MISC IDEAS FOR TESTING.
+* THEY ARE LEFT HERE TO SHOW WHOMEVER READS THIS FILE WHAT
+* WAS MY INTENTION AND/OR WHAT I WAS THINKING OF DOING WHILE
+* FINALIZING THIS PROJECT!!! :)*/
 
 /* public static boolean removeDuplicateWords(ArrayList<String> wordList) {
         for (int i = 0; i < wordList.size(); i++) {
@@ -67,15 +71,17 @@ public class CSC240_Week1MsgMapping {
 
         }
         return false;
-    } */
+    }*/
 
-///////////////////////////////////////////////////////////////////////////////
 
-/*public void showPatients(String name) {
-    boolean match = false;
-    for(matchingname : patientList) {
-        if (matchingname.toLowerCase.contains(name.toLowerCase())) {
-            match = true;
+/* private static void removeDuplicateWords(ArrayList<String> wordList) {
+        ArrayList<String> newList = new ArrayList<>();
+        for (int i = 0; i < wordList.size(); i++) {
+            for (int j = i + 1; j < wordList.size(); j++) {
+                if (wordList.get(i).equals(wordList.get(j))) {
+                    return;
+                }
+            }
+
         }
-    }
-} */
+    }*/
