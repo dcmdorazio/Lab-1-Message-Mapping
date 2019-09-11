29985e75540a887a305c2de6842f36a072229338
Cody D'Orazio <cxdorazio@gmail.com>
2019-09-11 00:42:42 -0400

    Initial Commit: 09/01/19
    Commit Log:
    09/01/19: Created Files for the initialization of the application.
    09/03/19: Code is re-arranged to accept the incoming file code.
    09/03/19: Had to start over because the code was incorrect.
    09/04/19: Moved 'Main' method from its own java class, to the MsgMapping.java class.
    09/09/19: Refurbished "runner.java"(MAIN METHOD). Implimented new methods
              to CSC240_Week1MsgMapping.java.
    09/10/19: Fixed the output once the user types their .txt file.
    09/10/19: Finished project
              Known errors: The counter returns '1' number short!

--- src/CSC240_Week1MsgMapping.java
+++ src/CSC240_Week1MsgMapping.java
@@ -7,12 +7,16 @@
  * Last Edited: 09/10/19
  */
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CSC240_Week1MsgMapping {
+
+    /* Initializes the "lineCounter" for the program to
+     *  print out the total number of messages in the given .txt file. */
+    private static int lineCounter = 0;  // this is a "java object"
+
 
     public static void main(String[] args) throws IOException {
         //First array in project
@@ -22,26 +26,57 @@ public class CSC240_Week1MsgMapping {
         Scanner c = new Scanner(System.in);
 
         /* The 3 lines of code below, are to take the users input to parse the .txt file
-        ** the user inputs with they keyboard*/
+         ** the user inputs with they keyboard*/
         System.out.print("Enter the name of the file you wish to use: ");
         String fileName = c.nextLine();
+
+        // Initializes the file path from the given .txt the user gives.
         File file = new File(fileName);
+       /* if (file.exists()) {
+            try {
+                FileReader fileReader = new FileReader(file);
+                LineNumberReader fR = new LineNumberReader(fileReader);
+                int lineNumberCount = 0;
+
+                    while(fileReader.getLineCounter() != null) {
+                        lineNumberCount ++;
+                    }
+                    System.out.println("test quote");
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }*/
+
+
+
+
+        /* THE PRINT FUNCTION BELOW WILL PRINT OUT THE FILE NAME THE USER
+         *  HAS CHOSEN TO BE READ AND USED IN THIS PROGRAM*/
+        System.out.println("\n" + "The name of the file you have chosen is: " + fileName + "\n");
 
         /*This scanner called "input" will be used to take
          * the users input into the machine for use.*/
         Scanner input = new Scanner(file);
-        //int counter = 0;
 
+        // This while loop will take the users input and view the words, and then adding them into the list
         while (input.hasNext()) {
+            //TODO: move the below "lineCounter"
+            //lineCounter++;
             list.add(input.next());
         }
-        ArrayList<String> nextList = CSC240_Week1MsgMapping.removeDups(list);
-        for(String x : nextList) {
+        System.out.println("These are the words the file, without the duplicates: ");
+        ArrayList<String> nextList = removeDups(list);
+        for (String x : nextList) {
             System.out.println(x);
         }
 
+        // This print statement should print the message count of the file given by the user.
+        // /*THIS NEEDS TO BE DEBUGGED*/
+        System.out.println("\n" + "The amount of Messages in " + fileName + " is: " + getLineCounter(fileName));
     }
 
+    /* This method will remove the duplicate words from the .txt file!*/
     private static <String> ArrayList<String> removeDups(ArrayList<String> list) {
         //Second array in project
         ArrayList<String> nextList = new ArrayList<>();
@@ -53,13 +88,34 @@ public class CSC240_Week1MsgMapping {
         }
         return nextList;
     }
+
+    /* This method is used to get the total amount
+    *  of lines in the users .txt file*/
+    private static int getLineCounter(String filename) throws IOException {
+        try (InputStream is = new BufferedInputStream(new FileInputStream(filename))) {
+            byte[] c = new byte[1024];
+            int lineCounter = 0;
+            int readChars = 0;
+            while ((readChars = is.read(c)) != -1) {
+                for (int i = 0; i < readChars; ++i) {
+                    if (c[i] == '\n') {
+                        lineCounter ++;
+                    }
+                }
+            }
+            int i = lineCounter;
+            return lineCounter;
+
+        }
+    }
+
 }
 
 /*THE COMMENTED CODE BELOW THIS LINE ARE TEST METHODS AND
-* OTHER MISC IDEAS FOR TESTING.
-* THEY ARE LEFT HERE TO SHOW WHOMEVER READS THIS FILE WHAT
-* WAS MY INTENTION AND/OR WHAT I WAS THINKING OF DOING WHILE
-* FINALIZING THIS PROJECT!!! :)*/
+ * OTHER MISC IDEAS FOR TESTING.                                     **** IMPORTANT, PLEASE READ!!! ****
+ * THEY ARE LEFT HERE TO SHOW WHOMEVER READS THIS FILE WHAT
+ * WAS MY INTENTION AND/OR WHAT I WAS THINKING OF DOING WHILE
+ * FINALIZING THIS PROJECT!!! :)*/
 
 /* public static boolean removeDuplicateWords(ArrayList<String> wordList) {
         for (int i = 0; i < wordList.size(); i++) {
@@ -85,3 +141,23 @@ public class CSC240_Week1MsgMapping {
 
         }
     }*/
+
+
+/*if (file.canRead()) {
+            try {
+                FileReader fr = new LineNumberReader(fr);
+                LineNumberReader lr = new LineNumberReader(fr);
+                int lineNumberCount = 0;
+
+                try {
+                    while (lr.readLine() != null) {
+                        lineNumberCount++;
+                    }
+                    System.out.println("Total number of messages in this file: " + lineNumberCount);
+                } catch (IOException a) {
+                    a.printStackTrace();
+                }
+            } catch (FileNotFoundException a) {
+                a.printStackTrace();
+            }
+        }*/
